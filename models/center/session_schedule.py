5e24a945cc93be07bdf862d9b8af8fc58e9a5ea5
Alfredo Oltra <aoltra@gmail.com>
2026-04-13 14:32:08 +0000

    fix: se añade un número al identificador del día para poder ordenar

--- models/center/session_schedule.py
+++ models/center/session_schedule.py
@@ -13,11 +13,11 @@ class SessionSchedule(models.Model):
 
   name = fields.Char(_('Descripción'), required = True, translate = True, help=_('Descripción de la sesión.'))
   week_day = fields.Selection([
-      ('L', _('Lunes')),
-      ('M', _('Martes')),
-      ('X', _('Miércoles')),
-      ('J', _('Jueves')),
-      ('V', _('Viernes')),
+      ('0L', _('Lunes')),
+      ('1M', _('Martes')),
+      ('2X', _('Miércoles')),
+      ('3J', _('Jueves')),
+      ('4V', _('Viernes')),
   ], string = _('Día de la semana'), help = _('Día de la semana de la sesión.'), required = True)
 
   start_time = fields.Float(
@@ -34,8 +34,7 @@ class SessionSchedule(models.Model):
 
   duration = fields.Float(
     string = _('Duración (Horas)'), 
-    compute = '_compute_duration', 
-    store = True
+    compute = '_compute_duration'
   )
 
   active = fields.Boolean('Activa', default=True)
@@ -57,19 +56,19 @@ class SessionSchedule(models.Model):
       """
       for record in self:
 
-          domain = [
-              # Ignora el registro que se está creando
-              ('id', '!=', record.id),                   
-              # Comprobar que sea en la misma localización y día de la semana
-              ('location_id', '=', record.location_id.id),
-              ('week_day', '=', record.week_day),  
-              # Comprobar solapamiento        
-              ('start_time', '<', record.end_time),        
-              ('end_time', '>', record.start_time),        
-          ]
-      
-          overlapping_sessions = self.search_count(domain)
+        domain = [
+          # Ignora el registro que se está creando
+          ('id', '!=', record.id),                   
+          # Comprobar que sea en la misma localización y día de la semana
+          ('location_id', '=', record.location_id.id),
+          ('week_day', '=', record.week_day),  
+          # Comprobar solapamiento        
+          ('start_time', '<', record.end_time),        
+          ('end_time', '>', record.start_time),        
+        ]
+    
+        overlapping_sessions = self.search_count(domain)
 
-          if overlapping_sessions > 0:
-              raise ValidationError(_("Error: Ya existe una sesión en esta ubicación y día que se solapa con este horario."))
+        if overlapping_sessions > 0:
+          raise ValidationError(_("Error: Ya existe una sesión en esta ubicación y día que se solapa con este horario."))
         
