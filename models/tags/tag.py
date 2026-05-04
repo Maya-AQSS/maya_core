d223ebb952baba2f04f3308074a374d6b1615e39
Daniel Moret <danielmoretrobledo@gmail.com>
2026-05-04 09:46:09 +0000

    feat: añadir traducción en el campo name de tags y un campo code orientado a la busqueda inteligente

--- models/tags/tag.py
+++ models/tags/tag.py
@@ -1,8 +1,16 @@
-from odoo import models, fields
+from odoo import models, fields, _
 
 class MayaCoreTag(models.Model):
     _name = 'maya_core.tag'
     _description = 'Etiquetas Generales'
 
-    name = fields.Char(string='Nombre de la etiqueta', required=True)
+    name = fields.Char(string=_('Nombre de la Etiqueta'), required=True)
     color = fields.Integer(string='Color')
+
+    code = fields.Char(
+        string='Código interno', 
+        help='Identificador único para lógica de negocio. No modificar.'
+    )
+    _sql_constraints = [
+        ('code_unique', 'unique(code)', '¡El código interno de la etiqueta debe ser único!')
+    ]
